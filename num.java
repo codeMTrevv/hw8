fdf67cf3cdeafecc5d38e1194c6eae750ac3fe9e
TrevlonHamer <trevlon.hamer@yahoo.com>
2021-02-14 01:39:52 +0000

    first commit

---
+++ num.java
@@ -0,0 +1,9 @@
+class num{
+	public static void main(String[] args){
+		int [] list = {1,2,3,4,5,6,7,8,9,10};
+		int sum = 0;
+
+		for( int digt : list){
+			sum = sum+digt;
+		}
+		System.out.println(sum);}}
